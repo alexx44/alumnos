b6ca9eb4cbeb4b8348a74850ead4774250839ea8
Juan Muñoz <alexx44@users.noreply.github.com>
2016-07-25 17:33:44 -0400

    espacios

--- Controller/scripts1.js
+++ Controller/scripts1.js
@@ -1,12 +1,10 @@
-var app = angular.module("myApp", []);
+var app = angular.module ("myApp", []);
 
- app.controller('AlumnosController', function($scope) {
-   $scope.alumnos=[
-                    {nombre:"Juan Blanco", telefono: "1234567890", curso:"Segundo ESO"},
-					{nombre:"Rosa Luxemburgo", telefono: "0987654321", curso:"Primero ESO"},
-					{nombre:"Alberto Herrera", telefono: "1122334455", curso:"Segundo ESO"},
-					{nombre:"Ana Mariño", telefono: "6677889900", curso:"Tercero ESO"}
-					];
+ app.controller ('AlumnosController', function($scope) {
+   $scope.alumnos = [{nombre: "Juan Blanco", telefono: "1234567890", curso: "Segundo ESO"},
+					{nombre: "Rosa Luxemburgo", telefono: "0987654321", curso: "Primero ESO"},
+					{nombre: "Alberto Herrera", telefono: "1122334455", curso: "Segundo ESO"},
+					{nombre: "Ana Mariño", telefono: "6677889900", curso: "Tercero ESO"}];
      
 $scope.Save = function(){
 
